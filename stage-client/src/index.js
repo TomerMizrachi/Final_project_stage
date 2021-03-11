d6cf35833b7ad1a3a2102536eb5e0dbad029010a
omerbensimon <omerbensimon3005@gmail.com>
2021-03-11 15:50:59 +0200

    video setup

--- stage-client/src/index.js
+++ stage-client/src/index.js
@@ -2,16 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import * as serviceWorker from './serviceWorker';
+
+const videoJsOptions = {
+    controls: true,
+    bigPlayButton: false,
+    width: 320,
+    height: 240,
+    fluid: false,
+    plugins: {
+        /*
+        // wavesurfer section is only needed when recording audio-only
+        wavesurfer: {
+            backend: 'WebAudio',
+            waveColor: '#36393b',
+            progressColor: 'black',
+            debug: true,
+            cursorWidth: 1,
+            msDisplayMax: 20,
+            hideScrollbar: true,
+            displayMilliseconds: true,
+            plugins: [
+                // enable microphone plugin
+                WaveSurfer.microphone.create({
+                    bufferSize: 4096,
+                    numberOfInputChannels: 1,
+                    numberOfOutputChannels: 1,
+                    constraints: {
+                        video: false,
+                        audio: true
+                    }
+                })
+            ]
+        },
+        */
+        record: {
+            audio: true,
+            video: true,
+            maxLength: 60000,
+            debug: true,
+            // convertEngine: 'ts-ebml'
+        }
+    }
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App { ...videoJsOptions }/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+//serviceWorker.unregister();
