f8112c104881abd18f9f507aa53091562f237243
omerbensimon <omerbensimon3005@gmail.com>
2021-05-22 09:47:00 +0300

    audio fix

--- stage-client/src/containers/Audio.js
+++ stage-client/src/containers/Audio.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import AudioAnalyser from "react-audio-analyser"
 import auditionText from './textForAudition.txt';
 import axios from 'axios'
+import hark from 'hark';
 
 export default class Aud extends Component {
     constructor(props) {
@@ -15,11 +16,17 @@ export default class Aud extends Component {
             sumSimilariyScore:0,
             sumExactScore:0,
             finalScore:{},
-            roleSpeaking:"NONE"
+            roleSpeaking:"NONE",
+            auto_record_active:false
         }
+
     }
 
-x
+    controlAudio(status) {
+        this.setState({
+            status:status,auto_record_active:!this._record_active
+        })
+    }
 
     calculateTotalScore(){
         //avg of sumSimilarty and sumExcat divided by length of sentence/2, taking into consideration the actorLine and actor has one line each. Should be replaced.
@@ -46,14 +53,59 @@ x
 
   
     componentDidMount() {
+        this.readText()
+        this.speaking = false;
+        this.speech_timeout = 0;
+        var react_comp = this
+        navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(function (camera) {
+            var speechEvents = hark(camera, {});
+            speechEvents.on('speaking', function () {
+                if (react_comp.state.auto_record_active == false) return;
+                if (react_comp.state.status !== 'recording') {
+                    react_comp.setState({status:"recording"})
+                };
+                console.log("react_comp.state.status", react_comp.state.status)
+                // if (!react_comp.recording) {
+                //     console.log('Toggle record one', react_comp.videoPlayer)
+                //     //react_comp.videoPlayer.recordToggle.handleClick()
 
+                // }
+                // if(react_comp.speaking == false){
+                //     react_comp.startSpeechTimestamp = new Date().getTime();
+                // }
+                react_comp.speaking = true;
+                console.log('started speaking!');
+                clearTimeout(react_comp.speech_timeout);
+
+            });
+            speechEvents.on('stopped_speaking', function () {
+                console.log(react_comp.state)
+                // if (react_comp.speaking == false) return;
+                react_comp.setState({status:"inactive",auto_record_active:"false"})  
+
+               react_comp.speech_timeout = setTimeout(function () {
+                    react_comp.speaking = false;
+                    console.log('Stopped speaking')
+                }, 3 * 1000);
+
+                // logging  
+                var seconds = 3;
+                (function looper() {
+                    console.log('Recording is going to be stopped in ' + seconds + ' seconds.');
+                    seconds--;
+                    if (seconds <= 0) {
+                        return;
+                    }
+                    setTimeout(looper, 1000);
+                })();
+
+            }
+            
+            );
+        });
     }
 
-    controlAudio(status) {
-        this.setState({
-            status
-        })
-    }
+
 
     changeScheme(e) {
         this.setState({
@@ -62,10 +114,10 @@ x
     }
 
     render() {
-        this.readText()
-        console.log(this.state.entireText)
+        // this.readText()
+        // console.log(this.state.entireText)
         const isFinishedText = this.state.finishedText;
-        console.log(this.state)
+        // console.log(this.state)
         const { status, audioSrc } = this.state; // audioType is also available
         const audioProps = {
             audioType: "audio/wav",
@@ -87,12 +139,13 @@ x
 
                 formData.append("file", e);
                 console.log("succ stop", e)
-
                 if (this.state.currentLineIterator<this.state.entireText.length){
 
                 axios.post("http://127.0.0.1:5000/speechToTextAudio", formData)
                     .then(res => {
+                        this.setState({status:"inactive",auto_record_active:"false"})
                         let resultTranscript = res.data.transcript
+                        console.log(resultTranscript)
                         let expectedText = this.state.entireText[this.state.currentLineIterator].replace('actor:','')
                         this.setState({currentLineIterator:this.state.currentLineIterator+1,roleSpeaking:"ACTOR",lineToRead:this.state.entireText[this.state.currentLineIterator]})
                         axios.get("http://127.0.0.1:12345/compare", {
@@ -108,7 +161,7 @@ x
                             }
                         }).then(res=>{ 
                             if (this.state.currentLineIterator<this.state.entireText.length){
-                                console.log(res.data.data)
+                                // console.log(res.data.data)
                                 var base64string=res.data.data
                                 var snd = new Audio("data:audio/wav;base64," + base64string);
                                 snd.play()
@@ -117,11 +170,14 @@ x
                                     this.setState({finishedText:true,finalScore:this.calculateTotalScore()})
 
                                 }
+
                             }
+                      
+
                             else{
                                 console.log("No more texts to read")
                                 this.setState({finishedText:true,finalScore:this.calculateTotalScore()})
-                                console.log(this.state.finalScore)
+                                // console.log(this.state.finalScore)
                             }
                         })
                         })
@@ -138,7 +194,7 @@ x
                 }
             },
             onRecordCallback: (e) => {
-                console.log("recording", e)
+                // console.log("recording", e)
             },
             errorCallback: (err) => {
                 console.log("error", err)
