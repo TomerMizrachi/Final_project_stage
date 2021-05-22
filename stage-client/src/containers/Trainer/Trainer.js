2f80a2988b03c6e5df20d55791e390e8b0c9db8a
michall.frankell@gmail.com <michall.frankell@gmail.com>
2021-05-22 17:43:51 +0300

    Added dashboard

--- stage-client/src/containers/Trainer/Trainer.js
+++ stage-client/src/containers/Trainer/Trainer.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import Layout from '@containers/Layout/Layout';
-import Banner from './Banner/Banner';
-import SearchBar from './Banner/SearchBar/SearchBar';
 import DashboardLayout from '@containers/DashboardLayout/DashboardLayout'
 
 import PracticeList from './PracticeList/PracticeList';
