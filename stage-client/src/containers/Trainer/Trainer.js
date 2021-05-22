0967c5c29ace633a464cb3ab7d53b2e070f3f0ca
TomerMizrachi <remot.15.m@gmail.com>
2021-05-22 17:46:02 +0300

    trainer

--- stage-client/src/containers/Trainer/Trainer.js
+++ stage-client/src/containers/Trainer/Trainer.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import Layout from '@containers/Layout/Layout';
-import Banner from './Banner/Banner';
-import SearchBar from './Banner/SearchBar/SearchBar';
 import DashboardLayout from '@containers/DashboardLayout/DashboardLayout'
-
 import PracticeList from './PracticeList/PracticeList';
 
 
