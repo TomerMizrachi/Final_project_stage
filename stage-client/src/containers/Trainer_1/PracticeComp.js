dc38dcc4d22df5f69f86b5c6434d264f8f007be3
omerbensimon <omerbensimon3005@gmail.com>
2021-05-25 11:38:05 +0300

    trainer props

--- stage-client/src/containers/Trainer_1/PracticeComp.js
+++ stage-client/src/containers/Trainer_1/PracticeComp.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import SiteConfig from '@config/site.config';
 import { Grid, Box } from '@material-ui/core';
 import StyledAuditionStep from './PracticeComp.styles';
 import { IconButton, LinkButton } from '@components/uielements/Button/Button';
-import Popup from 'reactjs-popup';
 import Filming from '../Filming'
-import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
+// import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme } from '@material-ui/core/styles';
 import Audio from '../Audio'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 
-export default function PracticeComp() {
-	const [openAudio, setAudioOpen] = React.useState(false);
-	const [openVideo, setVidioOpen] = React.useState(false);
+ function PracticeComp(props) {
+	useEffect(() => {
+		console.log("this is me", props)
+		// props.getMyAuditions(props.auth.user.id)
+	}, [])
+	
+	const [openAudio, setAudioOpen] = useState(false);
+	const [openVideo, setVidioOpen] = useState(false);
+	const [openText, setTextOpen] = useState(false);
 	const theme = useTheme();
 	const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
 	const handleAudioClickOpen = () => {
@@ -26,11 +33,17 @@ export default function PracticeComp() {
 	const handleVideoClickOpen = () => {
 		setVidioOpen(true);
 	};
+	const handleTextClickOpen = () => {
+		setTextOpen(true);
+	};
 	const handleClose = () => {
 		setAudioOpen(false);
 	};
 	const handleVideoClose = () => {
 		setVidioOpen(false);
+	};
+	const handleTextClose = () => {
+		setTextOpen(false);
 	};
 
 	return (
@@ -48,8 +61,32 @@ export default function PracticeComp() {
 							</div>
 							<Grid item >
 								<Box display="flex" alignItems="center" justifyContent="space-evenly" flex={1}>
-									<Popup trigger={<LinkButton className="heading4 orange bt-sm round" position="center" modal>View Text</LinkButton>}>
-									</Popup>
+									<LinkButton className="heading4 orange bt-sm round" position="center" onClick={handleTextClickOpen}>View Text</LinkButton>
+									<Dialog fullScreen={fullScreen}
+										open={openText}
+										onClose={handleTextClose}
+										aria-labelledby="responsive-dialog-title">
+										<DialogTitle id="responsive-dialog-title">{"View Text"}</DialogTitle>
+										<DialogContent>
+											<DialogContentText>
+												<div className="container">
+													<div className="header">
+														<IconButton className="accent static">
+															<i className="material-icons">assignment</i>
+														</IconButton>
+														<span className="title text-accent">Hi, welcome to your audio trainer! Now you can practice the audition text.</span>
+													</div>
+												</div>
+												<DialogContentText>
+													"To be or not to be, this is the question."
+          										</DialogContentText>
+											</DialogContentText>
+										</DialogContent>
+										{/* <DialogActions>
+											<Button autoFocus onClick={handleClose} color="primary">Disagree</Button>
+											<Button onClick={handleClose} color="primary" autoFocus>Agree</Button>
+										</DialogActions> */}
+									</Dialog>
 								</Box>
 							</Grid>
 
@@ -144,3 +181,18 @@ export default function PracticeComp() {
 		</Box>
 	)
 }
+PracticeComp.propTypes = {
+	// getMyAuditions: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	// actor: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+	auth: state.auth,
+	// actor: state.actor
+})
+
+export default connect(
+	mapStateToProps,
+	// { getMyAuditions }
+)(withRouter(PracticeComp))
