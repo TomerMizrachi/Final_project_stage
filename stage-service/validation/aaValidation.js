326174632c723d44ed86c365a6e98200475ec13a
TomerMizrachi <remot.15.m@gmail.com>
2021-05-29 19:52:28 +0300

    audition peek alert message

--- stage-service/validation/aaValidation.js
+++ stage-service/validation/aaValidation.js
@@ -9,7 +9,7 @@ const validateAAInput = (data) => {
     data.DM = !isEmpty(data.DM) ? data.DM : ""
     // Password checks
     if (Validator.isEmpty(data.audition_id)) {
-        errors.audition_id = "audition_id field is required"
+        errors.audition_id = "please choose specific audition before sending DM"
     }
     if (Validator.isEmpty(data.actor_id)) {
         errors.actor_id = "actor_id field is required"
