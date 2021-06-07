0bc704b4bcfe8afe79f35fff7cbf52e097451092
omerbensimon <omerbensimon3005@gmail.com>
2021-06-07 22:03:18 +0300

    video uploading

--- stage-client/src/containers/ActorProfile/Details/ShowReal.js
+++ stage-client/src/containers/ActorProfile/Details/ShowReal.js
@@ -7,16 +7,63 @@ import { LinkButton } from '@components/uielements/Button/Button';
 import { withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { getActorInfo } from '@actions/actorActions'
+import { useDropzone } from 'react-dropzone';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import { DropzoneDialogBase } from 'material-ui-dropzone';
 
 function ShowReal(props) {
     const [videos, setVideos] = useState([])
     useEffect(() => {
         setVideos(props.actor.profile.videos)
     }, [props.actor])
-    const uploadVideo = e => {
-        e.preventDefault()
+
+    const [open, setOpen] = React.useState(false);
+    const [fileObjects, setFileObjects] = React.useState([]);
+
+    const dialogTitle = () => (
+        <>
+            <span>Upload file</span>
+            <IconButton
+                style={{ right: '12px', top: '8px', position: 'absolute' }}
+                onClick={() => setOpen(false)}>
+                <CloseIcon />
+            </IconButton>
+        </>
+    );
+    const uploadVideo = (fileObjects) => {
+        fileObjects.map((fileObject) => {
+
+
+            // axios({
+            //     method: "get",
+            //     url: "http://localhost:8001/actor-audition/get_signed_url",
+            // }).then(function (response) {
+            //     var postURL = response.data.postURL;
+            //     var getURL = response.data.getURL;
+            //     delete axios.defaults.headers.common['Authorization']
+            //     axios({
+            //         method: "put",
+            //         url: postURL,
+            //         data: formData.get('file'),
+            //         headers: {
+            //             'Content-Type': 'video/mp4', "AllowedHeaders": "", 'Access-Control-Allow-Origin': ''
+            //         }
+            //     }).then(res => {
+            //         console.log("Response from s3")
+            //         // this.setState({ success: true });
+            //     }).catch(error => {
+            //         console.log(error);
+            //     })
+            //     console.log(getURL, postURL);
+            // }).catch(function (error) {
+            //     console.log(error);
+            // })
+        })
         console.log("upload video")
     }
+
     return (
         <StyledPracticeStep>
             <Grid container spacing={5} className="align">
@@ -29,8 +76,35 @@ function ShowReal(props) {
                             <Box display="flex" alignItems="center">
                                 <div className="heading4">SHOW REAL</div>
                             </Box>
-                            <LinkButton onClick={uploadVideo} className="accent round bt-sm outline">Upload Video</LinkButton>
+                            <LinkButton onClick={() => setOpen(true)} className="accent round bt-sm outline">Upload Video</LinkButton>
                         </Box>
+                        <div>
+                            <DropzoneDialogBase
+                                dialogTitle={dialogTitle()}
+                                // acceptedFiles={['image/*']}
+                                acceptedFiles={['video/*']}
+                                fileObjects={fileObjects}
+                                cancelButtonText={"cancel"}
+                                submitButtonText={"submit"}
+                                maxFileSize={5000000}
+                                open={open}
+                                onAdd={newFileObjs => {
+                                    console.log('onAdd', newFileObjs);
+                                    setFileObjects([].concat(fileObjects, newFileObjs));
+                                }}
+                                onDelete={deleteFileObj => {
+                                    console.log('onDelete', deleteFileObj);
+                                }}
+                                onClose={() => setOpen(false)}
+                                onSave={() => {
+                                    console.log('onSave', fileObjects);
+                                    uploadVideo(fileObjects)
+                                    setOpen(false);
+                                }}
+                                showPreviews={true}
+                                showFileNamesInPreview={true}
+                            />
+                        </div>
                         <div className="grid-container">
                             <div className="wrapper">
                                 <Grid container spacing={4}>
@@ -65,15 +139,3 @@ export default connect(
     mapStateToProps,
     { getActorInfo }
 )(withRouter(ShowReal))
-{/* <Grid container spacing={3} className="align">
-<Grid item xs={3}>
-</Grid>
-<Grid item xs={6}>
-    <div className="heading4">SHOW REAL</div>
-    <Grid item >
-        <Box mb={6}>
-            <ShowReal showReal={props.actor.profile.videos} />
-        </Box>
-    </Grid>
-</Grid>
-</Grid> */}
