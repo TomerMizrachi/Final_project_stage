39d875e5ef4f90e3bba01415f876d73f59b46eb3
MichalFrankel <48519009+MichalFrankel@users.noreply.github.com>
2021-08-31 18:49:37 +0300

    Update Audio.js
    Aligned with Heroku

--- stage-client/src/containers/Audio.js
+++ stage-client/src/containers/Audio.js
@@ -136,7 +136,7 @@ export default class Aud extends Component {
                 console.log("succ stop", e)
                 if (this.state.currentLineIterator < this.state.entireText.length) {
                     this.setState({ status: "inactive", auto_record_active: false })
-                    axios.post("http://127.0.0.1:5000/speechToTextAudio", formData)
+                    axios.post("https://textualservices.herokuapp.com/speechToTextAudio", formData)
                         .then(res => {
                             let confidence=res.data.confidence
                             let resultTranscript = res.data.transcript
@@ -148,14 +148,14 @@ export default class Aud extends Component {
                             console.log('Result transcript', resultTranscript)
                             let expectedText = this.state.entireText[this.state.currentLineIterator].replace('actor:', '')
                             this.setState({ currentLineIterator: this.state.currentLineIterator + 1, roleSpeaking: "ACTOR", lineToRead: this.state.entireText[this.state.currentLineIterator] })
-                            axios.get("http://127.0.0.1:12345/compare", {
+                            axios.get("https://sentencesimilaritystage.herokuapp.com/compare", {
                                 params: {
                                     inputText: resultTranscript,
                                     expectedText: expectedText
                                 }
                             }).then(res => {
                                 this.setState({ roleSpeaking: "VOCAL_SERVICE", sumExactScore: parseFloat(this.state.sumExactScore) + parseFloat(res.data.exactScore), sumSimilariyScore: parseFloat(this.state.sumSimilariyScore) + parseFloat(res.data.similarityScore) })
-                                axios.get("http://127.0.0.1:5000/textToSpeech", {
+                                axios.get("https://textualservices.herokuapp.com/textToSpeech", {
                                     params: {
                                         textToRead: this.state.entireText[this.state.currentLineIterator].replace('other actor:', '')
                                     }
